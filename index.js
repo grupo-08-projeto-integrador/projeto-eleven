c43339c88b7874d18447c49663b22dd1bf7a2f76
MatheuZAguiar <matheuspaguiar099@gmail.com>
2022-10-31 03:04:20 -0300

    UpdateProductPage

---
+++ index.js
@@ -0,0 +1,9 @@
+function contador(){
+    var contmais=document.getElementsByClassName("mais")
+    var contmenos=document.getElementsByClassName("menos")
+    if(contmais != null)
+        var cont=Number(cont+1);
+    if(contmenos != null)
+        var cont=Number(cont-1);
+    console.log(cont)
+}
