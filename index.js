a3d6bfcf474f84118f4114f352c83a8eff1097ae
MatheuZAguiar <matheuspaguiar099@gmail.com>
2022-11-01 02:15:28 -0300

    UpdateBotaoContador

--- index.js
+++
@@ -1,9 +0,0 @@
-function contador(){
-    var contmais=document.getElementsByClassName("mais")
-    var contmenos=document.getElementsByClassName("menos")
-    if(contmais != null)
-        var cont=Number(cont+1);
-    if(contmenos != null)
-        var cont=Number(cont-1);
-    console.log(cont)
-}
