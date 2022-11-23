5f740cff060aecc09ee4dc634e2760e61eeab5b1
Luiz Henz <robertgito@hotmail.com>
2022-11-23 19:34:10 -0300

    update

--- home2.js
+++ home2.js
@@ -1,3 +1,6 @@
-document.querySelector(".hamburger").addEventListener("click", () =>
+
+
+document.querySelector(".hamburguer").addEventListener("click", () =>
     document.querySelector(".container").classList.toggle("show-menu")
 );
+container
