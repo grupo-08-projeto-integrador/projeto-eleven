50f7499871b30d3c6e96e84c430165d323e470d6
MGPKrohn <matheuskrohn@gmail.com>
2022-11-27 16:25:58 -0300

    CorreçõesMediasMenusSlidersResponsividade

--- productpagehamb.js
+++ productpagehamb.js
@@ -3,3 +3,8 @@ function hamburguerOnClick() {
     document.getElementById("nav").classList.toggle("change");
     document.getElementById("menu-bg").classList.toggle("change-bg");
 }
+function hamburguerOnClick1() {
+    document.getElementById("hamburguer-menu1").classList.toggle("change");
+    document.getElementById("nav1").classList.toggle("change");
+    document.getElementById("menu-bg1").classList.toggle("change-bg");
+}
