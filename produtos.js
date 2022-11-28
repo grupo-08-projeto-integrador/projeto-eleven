6f057e413de2e5df735bb1096e23a39d9aee4c40
MatheuZAguiar <matheuspaguiar099@gmail.com>
2022-11-27 23:09:17 -0300

    TelasLinkadas

---
+++ produtos.js
@@ -0,0 +1,56 @@
+let produtos = [
+    {
+        nome:"T-Shirt HUGO BOSS Preta",
+        preco:300.00,
+        caminho_imagem:"imagens\camisetas\camiseta.JPG",
+        descricao:"Confortavel e Estilosa"
+    },
+    {
+        nome:"T-Shirt HUGO BOSS Azul",
+        preco:300.00,
+        caminho_imagem:"imagens\camisetas\camiseta1.JPG",
+        descricao:"Confortavel e Estilosa"
+    },
+    {
+        nome:"T-Shirt EA7 Preta",
+        preco:300.00,
+        caminho_imagem:"imagens\camisetas\camiseta2.JPG",
+        descricao:"Confortavel e Estilosa"
+    },
+    {
+        nome:"T-Shirt EA7 Preta/laranja",
+        preco:300.00,
+        caminho_imagem:"imagens\camisetas\camiseta3.JPG",
+        descricao:"Confortavel e Estilosa"
+    },
+    {
+        nome:"T-Shirt Porsche | BOSS",
+        preco:300.00,
+        caminho_imagem:"imagens\camisetas\camiseta4.JPG",
+        descricao:"Confortavel e Estilosa"
+    },
+    {
+        nome:"T-Shirt EA7 Preta/Branca",
+        preco:300.00,
+        caminho_imagem:"imagens\camisetas\camiseta5.JPG",
+        descricao:"Confortavel e Estilosa"
+    },
+    {
+        nome:"T-Shirt EA7 Azul",
+        preco:300.00,
+        caminho_imagem:"imagens\camisetas\camiseta6.JPG",
+        descricao:"Confortavel e Estilosa"
+    },
+    {
+        nome:"T-Shirt Armani Preta",
+        preco:300.00,
+        caminho_imagem:"imagens\camisetas\camiseta7.JPG",
+        descricao:"Confortavel e Estilosa"
+    },
+    {
+        nome:"T-Shirt Armani EA7 Preta",
+        preco:300.00,
+        caminho_imagem:"imagens\camisetas\camiseta8.JPG",
+        descricao:"Confortavel e Estilosa"
+    }
+]
