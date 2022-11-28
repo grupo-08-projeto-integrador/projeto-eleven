0cf455048f3bc2328a04318fb52b1c0f6b896e5d
MGPKrohn <matheuskrohn@gmail.com>
2022-11-28 16:12:38 -0300

    V1ADM

--- cadastro/loginJS.js
+++
@@ -1,5 +0,0 @@
-function hamburguerOnClick() {
-    document.getElementById("hamburguer-menu").classList.toggle("change");
-    document.getElementById("nav").classList.toggle("change");
-    document.getElementById("menu-bg").classList.toggle("change-bg");
-}
--- productpage/productpagehamb.js
+++
@@ -1,7 +0,0 @@
-function clickMenu(){
-    if(itensmenu.style.display == 'block'){
-    itensmenu.style.display = 'none';
-    } else {
-        itensmenu.style.display = 'block';
-    }
-}
