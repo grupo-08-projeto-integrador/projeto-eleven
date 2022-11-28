4b39e64490ab3aac2a72a27af8825d9d566bd895
MGPKrohn <matheuskrohn@gmail.com>
2022-11-28 02:22:12 -0300

    LinkadoPages

---
+++ cadastro/loginJS.js
@@ -0,0 +1,5 @@
+function hamburguerOnClick() {
+    document.getElementById("hamburguer-menu").classList.toggle("change");
+    document.getElementById("nav").classList.toggle("change");
+    document.getElementById("menu-bg").classList.toggle("change-bg");
+}
---
+++ contatos/productpagehamb.js
@@ -0,0 +1,10 @@
+function hamburguerOnClick() {
+    document.getElementById("hamburguer-menu").classList.toggle("change");
+    document.getElementById("nav").classList.toggle("change");
+    document.getElementById("menu-bg").classList.toggle("change-bg");
+}
+function hamburguerOnClick1() {
+    document.getElementById("hamburguer-menu1").classList.toggle("change");
+    document.getElementById("nav1").classList.toggle("change");
+    document.getElementById("menu-bg1").classList.toggle("change-bg");
+}
